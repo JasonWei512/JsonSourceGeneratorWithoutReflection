0c4a433b234166180cf873754d9b7960f3a61e49
魏杰 <16899918+JasonWei512@users.noreply.github.com>
2021-07-26 13:51:06 +0800

    Fix typo

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
-using System.Threading.Tasks;
 
-namespace JsonSourceGeneratorWIthoutReflection
+namespace JsonSourceGeneratorWithoutReflection
 {
     public class Program
     {
